0bb72f348a1c4955c8b792a945dbd16a39f99723
vicolachips44 <vga.nantes@gmail.com>
2015-06-03 18:31:20 +0200

    working on the expense editor

--- src/js/component/expenseEditor.js
+++ src/js/component/expenseEditor.js
@@ -20,17 +20,42 @@ function(ko) {
     _this = this;
 
     this.facade = params.facade;
-    //
+
     this.facade.logger.trace('in ExpenseEditor component constructor');
 
     this.editedExpense = ko.observable();
+
+    this.btnNewLabel    = this.facade.lng.get('menu/new');
+    this.btnSaveLabel   = this.facade.lng.get('menu/save');
+    this.btnDeleteLabel = this.facade.lng.get('menu/delete');
+
+    $('#exeBtnDelete').attr('disabled', 'disabled');
+    /**
+     * Subscribtion to the context value changing in facade object
+     * to update the UI.
+     */
+    this.facade.context.subscribe(function(newValue) {
+      if (newValue === 'welcome') {
+        $('#tca_expense_editor').show();
+      } else {
+        $('#tca_expense_editor').hide();
+      }
+    });
   }
 
   ExpenseEditor.prototype = {
     constructor: ExpenseEditor,
 
-    doSomething: function() {
-      alert('something...');
+    btnNewClick: function() {
+      _this.facade.logger.trace('btn new on expense editor was clicked...');
+    },
+
+    btnSaveClick: function() {
+      _this.facade.logger.trace('btn save on expense editor was clicked...');
+    },
+
+    btnDeleteClick: function() {
+      _this.facade.logger.trace('btn delete on expense editor was clicked...');
     }
   };
 
